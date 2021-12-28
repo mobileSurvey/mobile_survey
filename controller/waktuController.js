96731b908082eb0ace1548f8c3fd26d14c0fc6f4
deskabetakusuma <deskabetakusuma@gmail.com>
2021-12-28 22:00:04 +0700

    waktu

--- controller/waktuController.js
+++ controller/waktuController.js
@@ -3,65 +3,124 @@ const moment = require('moment')
 
 function createWaktu() {
     waktu.findOrCreate({
-      where: {
-        id: 1,
-      },
-      defaults: {},
+        where: {
+            id: 1,
+        },
+        defaults: {},
     });
-  }
-  createWaktu();
+}
+createWaktu();
 
 
-class Controller{
 
-    static updateWaktu(req,res){
-        const{waktuAwalDewan,waktuAkhirDewan,waktuAwalSurveyor,waktuAkhirSurveyor}=req.body
-        waktu.update({waktuAwalDewan,waktuAkhirDewan,waktuAwalSurveyor,waktuAkhirSurveyor},{where:{
-            id:1
-        }})
-        .then(hasil=>{
-            res.json("sukses")
+class Controller {
+
+    static edit(req, res) {
+        waktu.findAll({
+            where: {
+                id: 1
+            }
         })
-        .catch(err=>{
-            res.json(err)
-        })
+            .then(hasil => {
+        res.render('content-backoffice/jadwalBukaTutup/edit', { user: req.session.user, hasil });
+            })
+
     }
 
-    static checkWaktuDewan(req,res){
-        waktu.findAll({where:{
-            id:1
-        }})
-        .then(hasil=>{
-            if(hasil[0].waktuAwalDewan<=moment() && hasil[0].waktuAkhirDewan>=moment()){
-                res.status(200).json({ status: 200, message: true})
+    static updateWaktu(req, res) {
+        const { waktuAwalDewan, waktuAkhirDewan, waktuAwalSurveyor, waktuAkhirSurveyor } = req.body
+        waktu.update({ waktuAwalDewan, waktuAkhirDewan, waktuAwalSurveyor, waktuAkhirSurveyor }, {
+            where: {
+                id: 1
             }
-            else{
-                res.status(200).json({ status: 200, message: false})
-            }
-            
         })
-        .catch(err=>{
-            res.json(err)
-        })
+            .then(hasil => {
+                res.json("sukses")
+            })
+            .catch(err => {
+                res.json(err)
+            })
     }
 
-    static checkWaktuSurveyor(req,res){
-        waktu.findAll({where:{
-            id:1
-        }})
-        .then(hasil=>{
-            if(hasil[0].waktuAwalSurveyor<=moment() && hasil[0].waktuAkhirSurveyor>=moment()){
-                res.status(200).json({ status: 200, message: true})
+    static checkWaktuDewan(req, res) {
+        waktu.findAll({
+            where: {
+                id: 1
             }
-            else{
-                res.status(200).json({ status: 200, message: false})
+        })
+            .then(hasil => {
+                if (hasil[0].waktuAwalDewan <= moment() && hasil[0].waktuAkhirDewan >= moment()) {
+                    res.status(200).json({ status: 200, message: true })
+                }
+                else {
+                    res.status(200).json({ status: 200, message: false })
+                }
+
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    }
+
+    static checkWaktuSurveyor(req, res) {
+        waktu.findAll({
+            where: {
+                id: 1
             }
-            
         })
-        .catch(err=>{
-            res.json(err)
+            .then(hasil => {
+                if (hasil[0].waktuAwalSurveyor <= moment() && hasil[0].waktuAkhirSurveyor >= moment()) {
+                    res.status(200).json({ status: 200, message: true })
+                }
+                else {
+                    res.status(200).json({ status: 200, message: false })
+                }
+
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    }
+
+    static checkWaktuDewanf() {
+        waktu.findAll({
+            where: {
+                id: 1
+            }
         })
+            .then(hasil => {
+                if (hasil[0].waktuAwalDewan <= moment() && hasil[0].waktuAkhirDewan >= moment()) {
+                    return true;
+                }
+                else {
+                    return false
+                }
+
+            })
+            .catch(err => {
+                return err
+            })
+    }
+
+    static checkWaktuSurveyorf() {
+        waktu.findAll({
+            where: {
+                id: 1
+            }
+        })
+            .then(hasil => {
+                if (hasil[0].waktuAwalSurveyor <= moment() && hasil[0].waktuAkhirSurveyor >= moment()) {
+                    return true
+                }
+                else {
+                    return false
+                }
+
+            })
+            .catch(err => {
+                return err
+            })
     }
 }
 
-module.exports=Controller
+module.exports = Controller
--- routing/waktu.js
+++ routing/waktu.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const controller = require('../controller/waktuController')
-const {authentificationAdmin} = require('../middleware/authentification');
+const { authentificationAdmin } = require('../middleware/authentification');
 
-router.post('/updateWaktu',controller.updateWaktu)
-router.get('/checkWaktuDewan',controller.checkWaktuDewan)
-router.get('/checkWaktuSurveyor',controller.checkWaktuSurveyor)
+router.get('/edit/:id', controller.edit)
+
+router.post('/updateWaktu', controller.updateWaktu)
+router.get('/checkWaktuDewan', controller.checkWaktuDewan)
+router.get('/checkWaktuSurveyor', controller.checkWaktuSurveyor)
 
 
-module.exports=router
+module.exports = router
